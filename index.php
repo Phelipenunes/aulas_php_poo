93c0181b8a4811c4219e4c2f8cc0247fed97ab9a
Phelipe nunes <phelipesenac@gmail.com>
2023-09-21 11:57:28 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,26 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo</title>
+    <title>Exemplo 2 </title>
 </head>
 <body>
-    <h1>PHP com POO</h1>
+    <h1>PHP com POO - exemplo 2</h1>
     <hr>
     <h2>Assuntos aboradados:</h2>
     <ul>
-        <li>Criação de classe</li>
-        <li>Importando do arquivo de classe</li>
-        <li>Crian</li>
+        <li>Acesso direto ás propiedades</li>
+        <li>Atribuição e leitura de dados</li>
+        <li>Chamada de métodos</li>
     </ul>
 
 <?php
-//Importando a classe
 require_once "src/Cliente.php";
-
-//criando instancias da classe (objetos!)
 $cliente1 = new Cliente;
 $cliente2 = new Cliente;
-?>    
-<pre><?=var_dump($cliente1, $cliente2)?></pre>
+
+//Acesso e atribuição
+$cliente1->nome = "Pilip";
+$cliente1->telefones = ["11-1231-1232","11-6787-6754"];
+$cliente1->email = "pilip@gmail.com";
+$cliente1->senha =password_hash("1234pilip", PASSWORD_DEFAULT);
+
+$cliente2->nome = "Lucas";
+$cliente2->telefones = ["11-4531-1232","11-9087-6754"];
+$cliente2->email = "lucas@gmail.com";
+$cliente2->senha =password_hash("1234pilip", PASSWORD_DEFAULT);
+
+?>
+<hr>
+<h2>Dados dos objetos</h2>
+<h3><?=$cliente1->nome?></h3>
+<h3><?=$cliente2->nome?></h3>    
+
 </body>
 </html>
