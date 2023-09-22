99fab5afdb0deb3551356d5b305fc2f1bf577428
Phelipe nunes <phelipesenac@gmail.com>
2023-09-22 09:15:09 -0300

    .

--- index.php
+++ index.php
@@ -3,57 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 2 </title>
+    <title>Exemplo 3 </title>
 </head>
 <body>
-    <h1>PHP com POO - exemplo 2</h1>
+    <h1>PHP com POO - exemplo 3</h1>
     <hr>
     <h2>Assuntos aboradados:</h2>
     <ul>
-        <li>Acesso direto ás propiedades</li>
-        <li>Atribuição e leitura de dados</li>
-        <li>Chamada de métodos</li>
-        <li>Uso do<code>$this</code> para acessar recursos dentro da própia classe</li>
+        <li>método construtor com atribuição obrigatória de valores aos parâmetros/propiedades do objeto</li>
+        <li>Uso do <code>$this</code> para acesso às propiedades dentro do objeto</li>
     </ul>
 
 <?php
 require_once "src/Cliente.php";
-$cliente1 = new Cliente;
-$cliente2 = new Cliente;
+$cliente1 = new Cliente("Phelipe", "pilip@gmail.com",[11-95660-6752],[11-95660-6752]);
+$cliente2 = new Cliente("Lucas", "lucas@gmail.com",[11-95660-6752],[11-95660-6752]);
 
-//Acesso e atribuição
-$cliente1->nome = "Pilip";
-$cliente1->telefones = ["11-1231-1232","11-6787-6754"];
-$cliente1->email = "pilip@gmail.com";
-$cliente1->senha =password_hash("1234pilip", PASSWORD_DEFAULT);
 
-$cliente2->nome = "Lucas";
-$cliente2->telefones = ["11-4531-1232","11-9087-6754"];
-$cliente2->email = "lucas@gmail.com";
-$cliente2->senha =password_hash("1234pilip", PASSWORD_DEFAULT);
+//$cliente1->telefones = [11-95660-6752],[11-95660-6752]
+//$cliente2->telefones = [11-95660-6752],[11-95660-6752]
 
 ?>
-<hr>
-<h2>Dados dos objetos(Cliente 1)</h2>
-<h3>Nome:<?=$cliente1->nome?></h3>
-<h3>E-mail:<?=$cliente1->email?></h3>   
-<h3>Telefones:<?=$cliente1->telefones[0]?>,<?=$cliente1->telefones[1]?></h3>   
-
-<hr>
-<h2>Dados dos objetos (Cliente 2)</h2>
-<h3>Nome:<?=$cliente2->nome?></h3>
-<h3>E-mail:<?=$cliente2->email?></h3>   
-<h3>Telefones:<?=$cliente2->telefones[0]?>,<?=$cliente1->telefones[1]?></h3>
-
-<hr>
-
-<h2>Acessando dados através de um método </h2>
-<?=$cliente1->exibirdados()?>
-<?=$cliente2->exibirdados()?>
-
-  
-
-
 <pre><?=var_dump($cliente1,$cliente2)?></pre>
 </body>
 </html>
--- src/Cliente.php
+++ src/Cliente.php
@@ -1,17 +1,20 @@
 <?php
 class Cliente{
-    //propiedades(atributos)
+    
     public string $nome;
     public string $email;
     public string $senha;
     public array $telefones;
 
-    //Métodos (comportamentos)
-    public function exibirdados():void{
-        echo "<section>";
-        echo "<h2> $this->nome </h2>";
-        echo "<h2> $this->email </h2>";
-        echo "<section>";
+    //Método construtor
+    //Permite configurar a forma de inicialização do objeto, exigindo a atribuição de dados no momento em que criar o objeto, é sempre chamado de forma automática assim que o objeto for criado.
+
+    public function __construct(string $nome, string $email, array $telefones){
+        //Atribuindo os valores dos parâmetros às propiedades. Usamos o $this para acessar as propiedades desta classe.
+        $this->nome = $nome;
+        $this->email = $email;
+        $this->telefones = $telefones;
     }
 }
+
 
